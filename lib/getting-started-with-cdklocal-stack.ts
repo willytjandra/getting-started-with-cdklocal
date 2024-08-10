454b9faaa8d93342eac9f04831df5ccf161bd0de
willytjandra <willy.tjandra@gmail.com>
2024-08-10 11:12:01 +1000

    initial commit

--- lib/getting-started-with-cdklocal-stack.ts
+++ lib/getting-started-with-cdklocal-stack.ts
@@ -1,16 +1,21 @@
 import * as cdk from 'aws-cdk-lib';
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class GettingStartedWithCdklocalStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const ddbTable = new Table(this, 'DynamoDBTable', {
+      partitionKey: { name: "pk", type: AttributeType.STRING },
+      sortKey: { name: "sk", type: AttributeType.STRING },
+      tableName: 'your-dynamodb',
+      billingMode: BillingMode.PAY_PER_REQUEST,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'GettingStartedWithCdklocalQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new Bucket(this, 'Bucket', {
+      bucketName: 'your-bucket',
+    });
   }
 }
